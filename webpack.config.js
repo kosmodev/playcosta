6abcb782dc009a0b9cdc99e5e8208f34b4c1e95c
Luca Orlandini <luca.orlandini@docomodigital.com>
2018-10-05 22:22:31 +0200

    add webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
+// Import webpack moduke
+var webpack = require("webpack");
+// Import open browser plugin
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+//Import path module
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    entry: "./src/index.js", //set entry file
+    // Resolve to output directory and set file
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    // Add Url param to open browser plugin
+    plugins: [new OpenBrowserPlugin({url: 'http://localhost:3000'})],
+
+    // Set dev-server configuration
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3000
+    },
+
+    // Add babel-loader to transpile js and jsx files
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ["react"]
+                        }
+                    }
+                ]
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
